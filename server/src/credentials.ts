f3cc19438c7669a2f489f6fb8e5804d342f7ba42
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-09-21 10:41:22 -0300

    removed .env file

---
+++ server/src/credentials.ts
@@ -0,0 +1,5 @@
+export default {
+  DB:
+    "mongodb+srv://grochavieira:grochavieira1804@cluster.hrlm8.mongodb.net/AGENDA?retryWrites=true&w=majority",
+  PORT: "3333",
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import credentials from "./credentials";
 import mongoose from "mongoose";
 import routes from "./routes";
 
-dotenv.config();
-
-const db =
-  "mongodb+srv://grochavieira:grochavieira1804@cluster.hrlm8.mongodb.net/DEVNINJAS?retryWrites=true&w=majority";
-
 mongoose
-  .connect(db, {
+  .connect(credentials.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -27,6 +22,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening to port ${process.env.PORT}`);
+app.listen(credentials.PORT, () => {
+  console.log(`Listening to port ${credentials.PORT}`);
 });
